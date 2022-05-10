c602b96030e4c2825dbec88c40288d9878a24081
Dorian MOY <dorian.moy@epitech.eu>
2022-05-10 11:59:39 +0200

    [ADD] Color mode in tester.py

--- tests/tester.py
+++ tests/tester.py
@@ -15,7 +15,7 @@ def run_command(command: str, piped_path: str) -> None:
     return run(f'echo "{command}" | {piped_path}', shell=True, capture_output=True, text=True)
 
 
-def run_test(debug: bool, disp_all: bool) -> None:
+def run_test(debug: bool, disp_all: bool, color: bool) -> None:
     mysh = run_command(test["command"], "./42sh")
     tcsh = run_command(test["command"], "tcsh")
     name = test["name"]
@@ -24,11 +24,11 @@ def run_test(debug: bool, disp_all: bool) -> None:
         if debug:
             print(f"TCSH_out:{tcsh.stdout}\n42sh_out:{mysh.stdout}")
             print(f"TCSH_err:{tcsh.stderr}\n42sh_err:{mysh.stderr}")
-        print(colored(f'Test "{name}" failed.', "red"))
+        print(colored(f'Test "{name}" failed.', "red") if color else f'Test "{name}" failed.')
         return (-1)
     else:
         if disp_all:
-            print(colored(f'Test "{name}" passed.', "green"))
+            print(colored(f'Test "{name}" passed.', "green") if color else f'Test "{name}" passed.')
         return (1)
 
 
@@ -40,9 +40,10 @@ if __name__ == "__main__":
     nb_tests = len(json_data["tests"])
     disp_all = True if (len(argv) == 2 and "a" in argv[1]) else False
     debug = True if (len(argv) == 2 and "d" in argv[1]) else False
+    color = len(argv) == 2 and "c" in argv[1]
 
     for test in json_data["tests"]:
-        result = run_test(debug, disp_all)
+        result = run_test(debug, disp_all, color)
         nb_passed += 1 if result == 1 else 0
         nb_failed += 1 if result == -1 else 0
     print(f"Ran {nb_tests} tests ({nb_passed} passed and {nb_failed} failed).")
