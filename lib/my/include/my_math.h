be4522ff8bfdbe86ec87da38757923e7519f9680
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-05-10 15:33:06 +0200

    [RM] number lib functions

--- lib/my/include/my_math.h
+++ lib/my/include/my_math.h
@@ -9,11 +9,6 @@
 
 #include <stdbool.h>
 
-int my_nbrlen(int nbr);
-int my_lnbrlen(long lnbr);
 int my_getnbr(char const *str);
 int my_getnbr_base(char const *str, char const *base);
-char *my_parsenbr(int nbr);
-char *my_parsenbr_long(long long lnbr);
 bool my_is_number(char *str);
-char *my_number(char **str);
