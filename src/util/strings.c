57a9b34de2163b8b78737f56d8ce12c0245925d5
Dorian MOY <dorian.moy@epitech.eu>
2022-05-11 17:30:57 +0200

    [FIX] Bloat and logic in util/strings.c

--- src/util/strings.c
+++ src/util/strings.c
@@ -13,24 +13,19 @@
 
 char *get_next_argument(char *str, size_t index)
 {
-    bool text = false;
-    bool out = false;
     size_t size = strlen(str);
-    size_t i = index;
+    size_t i;
 
-    for (i = index; i < size && !out; i++) {
-        if (str[i] != ' ' && str[i] != '\t') {
-            text = true;
-        }
-        out = ((str[i] == ' ' || str[i] == '\t') && text);
-    }
+    for (i = index; i < size; i++)
+        if ((str[i] == ' ' || str[i] == '\t') &&
+            (str[i] != ' ' && str[i] != '\t'))
+            break;
     return (my_substr_size(str, (int) index, (int) (index + i), (int) size));
 }
 
 bool is_command_empty(command_t *command)
 {
-    size_t input = strlen(command->input);
     char *first = command->args[0];
 
-    return (input == 0 || first == NULL || strlen(first) == 0);
+    return (strlen(command->input) == 0 || first == NULL || strlen(first) == 0);
 }
