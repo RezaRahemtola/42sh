33d55987609fd61af442aad39a617630d549e7ed
Dorian MOY <dorian.moy@epitech.eu>
2022-05-11 16:04:49 +0200

    [FIX] Bloat and logic in builtin/silent.c

--- src/builtin/silent.c
+++ src/builtin/silent.c
@@ -22,12 +22,10 @@ int silent_exit(varenv_t **env, char **args, minishell_t *shell)
     if (size == 1) {
         shell->exit = 1;
         return (-1);
-    } else if (size > 2 || args[1][0] <= '0' || args[1][0] >= '9') {
+    } else if (size > 2 || args[1][0] <= '0' || args[1][0] >= '9')
         return (1);
-    }
-    if (!my_is_number(args[1])) {
+    if (!my_is_number(args[1]))
         return (1);
-    }
     shell->exit = 1;
     return (atoi(args[1]) % 256);
 }
@@ -45,11 +43,10 @@ int silent_setenv(varenv_t **env, char **args, minishell_t *shell)
     int size = my_arraylen(args);
 
     (void) shell;
-    if (size > 3 || (size >= 2 && !my_char_isalpha(args[1][0]))) {
+    if (size > 3 || (size >= 2 && !my_char_isalpha(args[1][0])))
         return (1);
-    } else if (size != 1) {
+    if (size != 1)
         return (set_variable(env, args[1], (size == 2 ? "" : args[2])));
-    }
     return (0);
 }
 
@@ -58,36 +55,33 @@ int silent_unsetenv(varenv_t **env, char **args, minishell_t *shell)
     int size = my_arraylen(args);
 
     (void) shell;
-    if (size == 1) {
+    if (size == 1)
         return (1);
-    } else if (size == 2 && strcmp(args[1], "*") == 0) {
+    if (size == 2 && strcmp(args[1], "*") == 0) {
         destroy_env(*env);
         *env = NULL;
         return (0);
     }
-    for (int i = 1; i < size; i++) {
+    for (int i = 1; i < size; i++)
         varenv_remove(env, args[i]);
-    }
     return (0);
 }
 
 int silent_cd(varenv_t **env, char **args, minishell_t *shell)
 {
-    int ret = 0;
+    int return_value;
     int size = my_arraylen(args);
     char *path = getcwd(NULL, 0);
 
     (void) shell;
-    if (path == NULL) {
+    if (path == NULL)
         return (1);
-    }
-    if (size == 1) {
-        ret = change_home_silently(env, path);
-    } else if (size == 2) {
-        ret = s_handle_cd(env, args[1], path);
-    } else {
-        ret = 1;
-    }
+    if (size == 1)
+        return_value = change_home_silently(env, path);
+    else if (size == 2)
+        return_value = s_handle_cd(env, args[1], path);
+    else
+        return_value = 1;
     free(path);
-    return (ret);
+    return (return_value);
 }
