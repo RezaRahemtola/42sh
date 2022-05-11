16853d2da1bc554b6dc4a2c29e84fae151a61124
Dorian MOY <dorian.moy@epitech.eu>
2022-05-11 17:35:14 +0200

    [FIX] Bloat and logic in varenv/variables.c

--- src/varenv/variables.c
+++ src/varenv/variables.c
@@ -13,17 +13,16 @@
 static int is_alphanumeric(char *str)
 {
     size_t size = strlen(str);
-    int number = 0;
-    int lower = 0;
-    int upper = 0;
+    int number;
+    int lower;
+    int upper;
 
     for (size_t i = 0; i < size; i++) {
         number = (str[i] >= '0' && str[i] <= '9');
         lower = (str[i] >= 'a' && str[i] <= 'z');
         upper = (str[i] >= 'A' && str[i] <= 'Z');
-        if (!number && !lower && !upper && str[i] != '_') {
+        if (!number && !lower && !upper && str[i] != '_')
             return (0);
-        }
     }
     return (1);
 }
@@ -32,11 +31,10 @@ void add_variable(varenv_t **env, char *key, char *value)
 {
     varenv_t *var = varenv_get(*env, key);
 
-    if (var == NULL) {
+    if (var == NULL)
         varenv_put(env, strdup(key), strdup(value));
-    } else {
+    else
         varenv_replace(*env, key, strdup(value));
-    }
 }
 
 int set_variable(varenv_t **env, char *key, char *value)
@@ -61,9 +59,8 @@ varenv_t *convert_env(char **env)
     for (int i = 0; env[i] != NULL; i++) {
         key = extract_key(env[i]);
         value = extract_value(env[i]);
-        if (key != NULL && value != NULL) {
+        if (key != NULL && value != NULL)
             varenv_put(&list, key, value);
-        }
     }
     return (list);
 }
@@ -79,7 +76,7 @@ char **convert_varenv(varenv_t *list)
         return NULL;
     while (current != NULL) {
         array[index] = malloc(sizeof(char) * (strlen(current->key) +
-        strlen(current->value) + 2));
+                                              strlen(current->value) + 2));
         sprintf(array[index], "%s=%s", current->key, current->value);
         index++;
         current = current->next;
