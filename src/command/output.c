4f34665803d1921724f5537ee91ae3788317ba24
Dorian MOY <dorian.moy@epitech.eu>
2022-05-11 16:49:08 +0200

    [FIX] Bloat and logic in command/output.c

--- src/command/output.c
+++ src/command/output.c
@@ -18,26 +18,26 @@ static bool check_null_ambiguous(command_t *command, char **array, int index)
     if (array[index + 1] == NULL || strlen(array[index + 1]) == 0) {
         fprintf(stderr, "%s\n", MISSING_NAME);
         return (false);
-    } else if (command->separator_out != NO_OUT) {
+    }
+    if (command->separator_out != NO_OUT) {
         fprintf(stderr, "%s\n", AMBIGUOUS_OUTPUT);
         return (false);
     }
     return (true);
 }
 
-bool check_out_append(command_t *command, char **array, int index, \
-varenv_t *env)
+bool check_out_append(command_t *command, char **array, int index,
+    varenv_t *env)
 {
     char **sub = NULL;
 
-    if (!check_null_ambiguous(command, array, index)) {
+    if (!check_null_ambiguous(command, array, index))
         return (false);
-    }
     command->separator_out = FILE_APPEND;
     sub = my_strsplit_many(array[index + 1], " \t");
-    if (sub == NULL) {
+    if (sub == NULL)
         return (false);
-    } else if (sub[0] == NULL || strlen(sub[0]) == 0) {
+    if (sub[0] == NULL || strlen(sub[0]) == 0) {
         fprintf(stderr, "%s\n", MISSING_NAME);
         my_free_arrays(1, sub);
         return (false);
@@ -53,14 +53,13 @@ varenv_t *env)
 {
     char **sub = NULL;
 
-    if (!check_null_ambiguous(command, array, index)) {
+    if (!check_null_ambiguous(command, array, index))
         return (false);
-    }
     command->separator_out = FILE_WRITE;
     sub = my_strsplit_many(array[index + 1], " \t");
-    if (sub == NULL) {
+    if (sub == NULL)
         return (false);
-    } else if (sub[0] == NULL || strlen(sub[0]) == 0) {
+    if (sub[0] == NULL || strlen(sub[0]) == 0) {
         fprintf(stderr, "%s\n", MISSING_NAME);
         my_free_arrays(1, sub);
         return (false);
