dce60a10f9a4e7ed0033942f4215b703f3cbc4d1
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-05-24 00:08:12 +0200

    [FIX] Wrong return values in str_contains

--- lib/my/src/string/my_str_contains.c
+++ lib/my/src/string/my_str_contains.c
@@ -14,14 +14,14 @@ bool my_str_contains(const char *str, const char *to_find)
     size_t same = 0;
 
     if (size_2 == 0)
-        return (0);
+        return (false);
     for (size_t i = 0; str[i] != '\0'; i++) {
         if (str[i] == to_find[same] && same + 1 == size_2)
-            return (false);
+            return (true);
         else
             same += (str[i] == to_find[same] ? 1 : str[i] == to_find[0]);
     }
-    return (true);
+    return (false);
 }
 
 bool my_str_containschar(const char *str, char c)
