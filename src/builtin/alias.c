afd8737026d1a45e28b821e7914defdb280194fd
Florian Lauch <florian.lauch@epitech.eu>
2022-05-25 22:35:32 +0200

    [ADD] Added full printing on alias

--- src/builtin/alias.c
+++ src/builtin/alias.c
@@ -5,13 +5,28 @@
 ** Alias builtin
 */
 
+#include <stdio.h>
 #include "environment.h"
+#include "my_arrays.h"
+
+static void print_aliases(env_t *aliases)
+{
+    env_t *current = aliases;
+
+    while (current != NULL) {
+        printf("%s\t%s\n", current->key, current->value);
+        current = current->next;
+    }
+}
 
 void builtin_alias(env_t **env, char *const *args, shell_t *shell)
 {
-    (void)shell;
-    (void)env;
-    (void)args;
+    (void) env;
+    size_t size = my_arraylen(args);
+
+    if (size == 0) {
+        print_aliases(shell->aliases);
+    }
 }
 
 int silent_alias(env_t **env, char *const *args, shell_t *shell)
