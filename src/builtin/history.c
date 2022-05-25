32c21d9a45bbb335b0558be19d0d980b3445fc39
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-05-25 16:45:59 +0200

    [FIX] History builtin working with redirections and pipes

--- src/builtin/history.c
+++ src/builtin/history.c
@@ -10,22 +10,21 @@
 
 void builtin_history(shell_t *shell, char *const *args)
 {
-    (void)shell;
-    (void)args;
-}
-
-int silent_history(shell_t *shell, char *const *args)
-{
     size_t size = my_list_size(shell->history);
     list_t *history = shell->history;
     history_t *elem = NULL;
 
-    (void)shell;
     (void)args;
     for (size_t i = 0; i < size; i++) {
         elem = history->data;
         printf("    %ld  %s  %s\n", elem->index, elem->time, elem->command);
         history = history->next;
     }
+}
+
+int silent_history(shell_t *shell, char *const *args)
+{
+    (void)shell;
+    (void)args;
     return (0);
 }
