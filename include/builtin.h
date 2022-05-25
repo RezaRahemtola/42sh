254950832b38778bdd7703dec8f32f90949ce84e
Florian Lauch <florian.lauch@epitech.eu>
2022-05-25 16:47:22 +0200

    [ADD] Added alias in builtin list

--- include/builtin.h
+++ include/builtin.h
@@ -15,6 +15,7 @@ void builtin_exit(env_t **env, char *const *args);
 void builtin_setenv(env_t **env, char *const *args);
 void builtin_unsetenv(env_t **env, char *const *args);
 void builtin_history(env_t **env, char *const *args);
+void builtin_alias(env_t **env, char *const *args);
 
 int silent_cd(env_t **env, char *const *args, shell_t *shell);
 int silent_env(env_t **env, char *const *args, shell_t *shell);
@@ -22,15 +23,17 @@ int silent_exit(env_t **env, char *const *args, shell_t *shell);
 int silent_setenv(env_t **env, char *const *args, shell_t *shell);
 int silent_unsetenv(env_t **env, char *const *args, shell_t *shell);
 int silent_history(env_t **env, char *const *args, shell_t *shell);
+int silent_alias(env_t **env, char *const *args, shell_t *shell);
 
 bool is_builtin(const char *command);
 
-static const builtin_t BUILTIN[7] = {
+static const builtin_t BUILTIN[8] = {
         { "exit", &builtin_exit, &silent_exit },
         { "env", &builtin_env, &silent_env },
         { "setenv", &builtin_setenv, &silent_setenv },
         { "unsetenv", &builtin_unsetenv, &silent_unsetenv },
         { "cd", &builtin_cd, &silent_cd },
         { "history", &builtin_history, &silent_history },
+        { "alias", &builtin_alias, &silent_alias },
         { 0, 0, 0 }
 };
---
+++ src/builtin/alias.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2022
+** 42sh
+** File description:
+** Alias builtin
+*/
+
+#include "environment.h"
+
+void builtin_alias(env_t **env, char *const *args)
+{
+    (void)env;
+    (void)args;
+}
+
+int silent_alias(env_t **env, char *const *args, shell_t *shell)
+{
+    (void) env;
+    (void) args;
+    (void) shell;
+    return (0);
+}
