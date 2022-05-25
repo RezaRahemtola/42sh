300db00699ceb207a02fa1f70660c1340957ec97
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-05-25 22:51:31 +0200

    [ADD] set multi var

--- src/builtin/localenv.c
+++ src/builtin/localenv.c
@@ -21,18 +21,25 @@ void builtin_set(shell_t *shell, char *const *args)
         print_localenv(shell->localenv);
         return;
     }
-    if (size == 4 && strcmp(args[2], "=") == 0 && !isalpha(args[1][0])) {
-        fprintf(stderr, "set: Variable name must begin with a letter.\n");
+    if (size == 4 && strcmp(args[2], "=") == 0) {
+        if (!isalpha(args[1][0]))
+            fprintf(stderr, "set: Variable name must begin with a letter.\n");
+        if (isalpha(args[1][0]) && !my_isalphanum_str(args[1]))
+            fprintf(stderr, "set: Variable name must %s\n", NONALPHA);
         return;
     }
-    if (size == 4 && strcmp(args[2], "=") == 0 && !my_isalphanum_str(args[1])) {
-        fprintf(stderr, "set: Variable name must %s\n", NONALPHA);
-        return;
+    for (size_t i = 1; i < size; i++) {
+        if (!my_isalphanum_str(args[i])) {
+            fprintf(stderr, "set: Variable name must %s\n", NONALPHA);
+            return;
+        }
     }
 }
 
 void builtin_unset(shell_t *shell, char *const *args)
 {
+    (void) shell;
+    (void) args;
     return;
 }
 
@@ -43,13 +50,20 @@ int silent_set(shell_t *shell, char *const *args)
     if (size == 4 && strcmp(args[2], "=") == 0) {
         if (!isalpha(args[1][0]) || !my_isalphanum_str(args[1]))
             return (1);
-        else
-            add_localvar(&shell->localenv, args[1], args[3], false);
+        add_localvar(&shell->localenv, args[1], args[3], false);
+        return (0);
     }
+    for (size_t i = 1; i < size; i++)
+        if (!my_isalphanum_str(args[i]))
+            return (1);
+    for (size_t i = 1; i < size; i++)
+        add_localvar(&shell->localenv, args[i], "", false);
     return (0);
 }
 
 int silent_unset(shell_t *shell, char *const *args)
 {
+    (void) shell;
+    (void) args;
     return (0);
 }
