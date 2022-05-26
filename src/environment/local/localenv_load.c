ab5d0e0136582ca31f10523d827186bb2ee0181f
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-05-26 15:31:09 +0200

    [ADD] gid and group localenv preload with error handling

--- src/environment/local/localenv_load.c
+++ src/environment/local/localenv_load.c
@@ -7,12 +7,13 @@
 
 #include <sys/types.h>
 #include <unistd.h>
+#include <grp.h>
 #include <pwd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "environment.h"
 
-static void add_uid_var(uid_t id, const char *key, shell_t *shell)
+static void add_id_var(unsigned int id, const char *key, shell_t *shell)
 {
     size_t id_len = snprintf(NULL, 0, "%d", id) + 1;
     char *value = malloc(sizeof(char) * id_len);
@@ -28,10 +29,18 @@ void load_localenv(shell_t *shell)
     uid_t uid = getuid();
     struct passwd *epw = getpwuid(euid);
     struct passwd *pw = getpwuid(uid);
+    struct group *group = NULL;
 
-    add_uid_var(euid, "euid", shell);
-    add_localvar(&shell->localenv, "euser", epw->pw_name, false);
-    add_localvar(&shell->localenv, "home", pw->pw_dir, false);
-    add_uid_var(uid, "uid", shell);
-    add_localvar(&shell->localenv, "user", pw->pw_name, false);
+    if (epw != NULL) {
+        add_id_var(euid, "euid", shell);
+        add_localvar(&shell->localenv, "euser", epw->pw_name, false);
+    }
+    if (pw != NULL) {
+        group = getgrgid(pw->pw_gid);
+        add_id_var(pw->pw_gid, "gid", shell);
+        add_localvar(&shell->localenv, "group", group->gr_name, false);
+        add_localvar(&shell->localenv, "home", pw->pw_dir, false);
+        add_id_var(uid, "uid", shell);
+        add_localvar(&shell->localenv, "user", pw->pw_name, false);
+    }
 }
