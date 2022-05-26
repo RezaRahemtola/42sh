598a5c8997dc3f2dd43a8dfdca7343e6e6e87cfe
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-05-26 23:05:01 +0200

    [ADD] Advanced history tests and fixes

--- tests/src/builtin/tests_history.c
+++ tests/src/builtin/tests_history.c
@@ -20,3 +20,91 @@ Test(history, simple, .init=cr_redirect_stdout)
     builtin_history(&shell, args);
     cr_assert_eq(silent_history(&shell, args), 0);
 }
+
+Test(history, last_empty, .init=cr_redirect_stderr)
+{
+    char *input = strdup("!!\n");
+    shell_t shell = {0, 0, NULL, NULL, NULL, NULL};
+
+    replace_history(&input, &shell);
+    cr_assert_stderr_eq_str("0: Event not found.\n");
+    cr_assert_str_eq(input, " ");
+    free(input);
+}
+
+Test(history, last, .init=cr_redirect_stdout)
+{
+    char *input = strdup("!!\n");
+    shell_t shell = {0, 0, NULL, NULL, NULL, NULL};
+
+    add_history("ls -la\n", &shell);
+    replace_history(&input, &shell);
+    cr_assert_str_eq(input, "ls -la\n");
+    free(input);
+}
+
+Test(history, by_index_wrong, .init=cr_redirect_stderr)
+{
+    char *input = strdup("!3\n");
+    shell_t shell = {0, 0, NULL, NULL, NULL, NULL};
+
+    add_history("ls -la\n", &shell);
+    replace_history(&input, &shell);
+    cr_assert_stderr_eq_str("3: Event not found.\n");
+    cr_assert_str_eq(input, " ");
+    free(input);
+}
+
+Test(history, by_index, .init=cr_redirect_stdout)
+{
+    char *input = strdup("!1\n");
+    shell_t shell = {0, 0, NULL, NULL, NULL, NULL};
+
+    add_history("ls -la\n", &shell);
+    replace_history(&input, &shell);
+    cr_assert_str_eq(input, "ls -la\n");
+    free(input);
+}
+
+Test(history, by_str_wrong, .init=cr_redirect_stderr)
+{
+    char *input = strdup("!cd\n");
+    shell_t shell = {0, 0, NULL, NULL, NULL, NULL};
+
+    add_history("ls -la\n", &shell);
+    replace_history(&input, &shell);
+    cr_assert_stderr_eq_str("cd: Event not found.\n");
+    free(input);
+}
+
+Test(history, by_str, .init=cr_redirect_stdout)
+{
+    char *input = strdup("!ls\n");
+    shell_t shell = {0, 0, NULL, NULL, NULL, NULL};
+
+    add_history("ls -la\n", &shell);
+    replace_history(&input, &shell);
+    cr_assert_str_eq(input, "ls -la\n");
+    free(input);
+}
+
+Test(history, by_backward_index_wrong, .init=cr_redirect_stderr)
+{
+    char *input = strdup("!-42\n");
+    shell_t shell = {0, 0, NULL, NULL, NULL, NULL};
+
+    replace_history(&input, &shell);
+    cr_assert_stderr_eq_str("-42: Event not found.\n");
+    free(input);
+}
+
+Test(history, by_backward_index, .init=cr_redirect_stdout)
+{
+    char *input = strdup("!-1\n");
+    shell_t shell = {0, 0, NULL, NULL, NULL, NULL};
+
+    add_history("ls -la\n", &shell);
+    replace_history(&input, &shell);
+    cr_assert_str_eq(input, "ls -la\n");
+    free(input);
+}
