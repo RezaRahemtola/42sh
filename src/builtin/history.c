3eb71aeddabd5f67195161d350fa615d40e45eb4
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-05-29 19:00:53 +0200

    [ADD] `history nb` with localenv history var support

--- src/builtin/history.c
+++ src/builtin/history.c
@@ -7,11 +7,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "shell.h"
 #include "history.h"
+#include "environment.h"
 #include "messages.h"
 #include "my_arrays.h"
 #include "my_math.h"
+#include "my.h"
 
 static bool is_history_error(char *const *args, bool print)
 {
@@ -30,9 +33,25 @@ static bool is_history_error(char *const *args, bool print)
     return (false);
 }
 
-static void display_nb_history(list_t *history, char *const *args)
+static int compute_max_history(shell_t *shell, const char *arg)
 {
-    size_t nb = atoi(args[1]) == 0 ? 1 : atoi(args[1]);
+    const localenv_t *env = get_localenv_value(shell->localenv, "history");
+    int len = my_list_size(shell->history);
+    int nb = (arg != NULL ? (atoi(arg) == 0 ? 1 : atoi(arg)) : 0);
+    int env_max = 0;
+
+    if (env == NULL && arg == NULL)
+        return (MIN(len, INT_MAX));
+    if (env == NULL && arg != NULL)
+        return (MIN(len, nb));
+    env_max = atoi(env->value) == 0 ? 1 : atoi(env->value);
+    if (arg == NULL && env != NULL)
+        return (MIN(len, env_max));
+    return (MIN(len, MIN(env_max, nb)));
+}
+
+static void display_nb_history(list_t *history, int nb)
+{
     history_t *elem = get_history(history, -nb);
 
     for (; nb > 0 && elem != NULL; nb--) {
@@ -44,20 +63,14 @@ static void display_nb_history(list_t *history, char *const *args)
 void builtin_history(shell_t *shell, char *const *args)
 {
     list_t *history = shell->history;
-    history_t *elem = NULL;
     size_t size = my_arraylen(args);
+    int nb = 0;
+    const char *arg = (size == 2 ? args[1] : NULL);
 
     if (is_history_error(args, true))
         return;
-    if (size == 2) {
-        display_nb_history(history, args);
-        return;
-    }
-    while (history != NULL) {
-        elem = history->data;
-        printf("    %ld  %s  %s\n", elem->index, elem->time, elem->command);
-        history = history->next;
-    }
+    nb = compute_max_history(shell, arg);
+    display_nb_history(history, nb);
 }
 
 int silent_history(shell_t *shell, char *const *args)
