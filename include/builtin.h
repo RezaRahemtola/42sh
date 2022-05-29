48182f18c6fe34441ee56fcb2c87a569b7058655
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2022-05-29 17:01:14 +0200

    [ADD] Working fg

--- include/builtin.h
+++ include/builtin.h
@@ -16,7 +16,8 @@ void builtin_setenv(env_t **env, char *const *args);
 void builtin_unsetenv(env_t **env, char *const *args);
 
 int silent_cd(env_t **env, char *const *args, shell_t *shell);
-void silent_job(env_t **env, char *const *args, shell_t *shell);
+int silent_fg(env_t **env, char *const *args, shell_t *shell);
+int silent_job(env_t **env, char *const *args, shell_t *shell);
 int silent_env(env_t **env, char *const *args, shell_t *shell);
 int silent_exit(env_t **env, char *const *args, shell_t *shell);
 int silent_setenv(env_t **env, char *const *args, shell_t *shell);
@@ -24,12 +25,13 @@ int silent_unsetenv(env_t **env, char *const *args, shell_t *shell);
 
 bool is_builtin(const char *command);
 
-static const builtin_t BUILTIN[7] = {
+static const builtin_t BUILTIN[8] = {
         { "exit", &builtin_exit, &silent_exit },
         { "env", &builtin_env, &silent_env },
         { "setenv", &builtin_setenv, &silent_setenv },
         { "unsetenv", &builtin_unsetenv, &silent_unsetenv },
         { "cd", &builtin_cd, &silent_cd },
         { "jobs", 0, &silent_job },
+        { "fg", 0, &silent_fg },
         { 0, 0, 0 }
 };
--- src/job_control/builtin_job.c
+++ src/job_control/builtin_job.c
@@ -8,13 +8,51 @@
 #include "types.h"
 #include <stdio.h>
 
-void silent_job(env_t **env, char *const *args, shell_t *shell)
+int silent_job(env_t **env, char *const *args, shell_t *shell)
 {
     job_t *job = shell->job;
+    size_t size = my_arraylen(args);
 
+    if (size != 1) {
+        fprintf(stderr, "jobs: Too many arguments.\n");
+        return (1);
+    }
     while (job != NULL) {
         printf("[%d]\tRunnig\t%s\n", job->nb_job, job->command);
         job = job->next;
     }
+    return (0);
 }
 
+int wait_fg(shell_t *shell, pid_t pid)
+{
+    int status = 0;
+
+    tcsetpgrp(0, pid);
+    waitpid(pid, &status, WUNTRACED | WCONTINUED);
+    handle_errors(status);
+    return (status > 255 ? status / 256 : status);
+}
+
+int silent_fg(env_t **env, char *const *args, shell_t *shell)
+{
+    job_t *job = shell->job;
+    size_t size = my_arraylen(args);
+    bool found = false;
+    int pid = 0;
+
+    if (size != 2) {
+        fprintf(stderr, "fg [PID].\n");
+        return (1);
+    }
+    pid = atoi(args[1]);
+    while (job != NULL) {
+        if (pid == job->pid)
+            found = true;
+        job = job->next;
+    }
+    if (found)
+        return (wait_fg(shell, pid));
+    fprintf(stderr, "fg: job not found: %d.\n", pid);
+    return (1);
+}
