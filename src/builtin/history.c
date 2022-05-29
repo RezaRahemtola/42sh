e803b7e6fb051176def3e70a338b0df7f83222aa
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-05-29 15:40:55 +0200

    [ADD] `history nb` started with error handling and tests

--- src/builtin/history.c
+++ src/builtin/history.c
@@ -7,15 +7,31 @@
 
 #include <stdio.h>
 #include "shell.h"
+#include "messages.h"
+#include "my_arrays.h"
+#include "my_math.h"
+
+static bool is_history_error(char *const *args, bool print)
+{
+    size_t size = my_arraylen(args);
+
+    if (size >= 2 && !my_is_number(args[1])) {
+        if (print)
+            fprintf(stderr, "history: %s\n", NUMBER_ERROR);
+        return (true);
+    }
+    return (false);
+}
 
 void builtin_history(shell_t *shell, char *const *args)
 {
-    size_t size = my_list_size(shell->history);
     list_t *history = shell->history;
     history_t *elem = NULL;
 
-    (void)args;
-    for (size_t i = 0; i < size; i++) {
+    (void) shell;
+    if (is_history_error(args, true))
+        return;
+    while (history != NULL) {
         elem = history->data;
         printf("    %ld  %s  %s\n", elem->index, elem->time, elem->command);
         history = history->next;
@@ -24,7 +40,8 @@ void builtin_history(shell_t *shell, char *const *args)
 
 int silent_history(shell_t *shell, char *const *args)
 {
-    (void)shell;
-    (void)args;
+    (void) shell;
+    if (is_history_error(args, false))
+        return (1);
     return (0);
 }
